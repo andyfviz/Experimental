b5f31f9a248d8e7107b6e3b2a7d6a6a9ad896dd1
andyfviz <afloresv@centro.edu.mx>
2026-02-16 23:22:03 -0600

    Toggle theme

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-console.log ("coco")
 
-//para subir a la nube: git status-->git add-->git commit -m"""-->git push 
+const toggleButton = document.getElementById("Theme");
+const body = document.body;
+
+const savedTheme =localStorage.getItem("Theme");
+
+if (savedTheme==="Dark"){
+    body.classList.add("dark-mode");
+    toggleButton.textContent="Light";
+    } else {
+    toggleButton.textContent = "Dark";
+}
+/*Click*/ 
+toggleButton.addEventListener ("click",() => {
+    body.classList.toggle("dark-mode");
+
+    if(body.classList.contains("dark-mode")){
+        localStorage.setItem("Theme", "Dark");
+        toggleButton.textContent ="Light";
+    } else{
+        localStorage.setItem("Theme","Light");
+        toggleButton.textContent= "Dark";
+    }
+});
