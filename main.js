4473dcca1ec0fcd2608cdb65edb25ea6ad9dd20b
andyfviz <afloresv@centro.edu.mx>
2026-02-20 00:11:49 -0600

    Tarjetas de Producto

--- main.js
+++ main.js
@@ -22,3 +22,27 @@ toggleButton.addEventListener ("click",() => {
         toggleButton.textContent= "Dark";
     }
 });
+
+const Tarjetas = document.querySelector(".Tarjetas");
+const card = document.querySelectorAll (".card");
+card [0].classList.add("active");
+
+function updateActiveCard() {
+ const carouselRect = carousel.getBoundingClientRect();
+    const carouselCenter = carouselRect.left + carouselRect.width / 2;
+
+    card.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        const cardCenter = rect.left + rect.width / 2;
+
+        const distance = Math.abs(carouselCenter - cardCenter);
+
+        if (distance < rect.width / 2) {
+            card.classList.add("active");
+        } else {
+            card.classList.remove("active");
+        }
+    });
+}
+
+carousel.addEventListener("scroll", updateActiveCard);
