a90bddbf2a490eb153deb8df55d12957a2335543
Andrea Flores <afloresv@centro.edu.mx>
2026-02-24 00:18:24 -0600

    Pomodoro

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
+/**
+ * RETO 1: CAMBIO DE TEMA 
+ */
 const toggleButton = document.getElementById("Theme");
 const body = document.body;
 
@@ -22,19 +25,22 @@ toggleButton.addEventListener ("click",() => {
         toggleButton.textContent= "Dark";
     }
 });
+/**
+ * RETO 2: Imagenes Carrusel 
+ */
+const carousel = document.querySelector(".Tarjetas");
+const cards = document.querySelectorAll(".card");
 
-const Tarjetas = document.querySelector(".Tarjetas");
-const card = document.querySelectorAll (".card");
-card [0].classList.add("active");
+// Activar la primera al inicio
+cards[0].classList.add("active");
 
 function updateActiveCard() {
- const carouselRect = carousel.getBoundingClientRect();
+    const carouselRect = carousel.getBoundingClientRect();
     const carouselCenter = carouselRect.left + carouselRect.width / 2;
 
-    card.forEach(card => {
+    cards.forEach(card => {
         const rect = card.getBoundingClientRect();
         const cardCenter = rect.left + rect.width / 2;
-
         const distance = Math.abs(carouselCenter - cardCenter);
 
         if (distance < rect.width / 2) {
@@ -46,3 +52,91 @@ function updateActiveCard() {
 }
 
 carousel.addEventListener("scroll", updateActiveCard);
+
+/**
+ * RETO 3: Pomodoro
+ */
+const timerDisplay = document.getElementById("timer");
+const startBtn = document.getElementById("start");
+const resetBtn = document.getElementById("reset");
+const modeLabel = document.getElementById("mode-label");
+const alarm = document.getElementById("alarm");
+
+let timer = null;
+let timeLeft = 1500;
+let isRunning = false;
+let mode = "Working Time";
+let sessionCount = 0;
+
+function updateDisplay() {
+    const minutes = Math.floor(timeLeft/60);
+    const seconds = timeLeft % 60;
+
+    timerDisplay.textContent = 
+    `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+}
+
+function startTimer() {
+    if (isRunning) return;
+    isRunning = true;
+
+    timer = setInterval(() => {
+        timeLeft--;
+        updateDisplay();
+
+        if (timeLeft <= 0){
+            clearInterval(timer);
+            isRunning = false;
+            alarm.onplay();
+            switchMode();
+        }
+    },1000);
+}
+function pauseTimer() {
+    clearInterval(timer);
+    isRunning = false;
+}
+function resetTimer(){
+    clearInterval(timer);
+    isRunning = false;
+    setMode(mode);
+}
+function switchMode() {
+    if (mode==="Working Time") {
+        sessionCount++;
+
+        if (sessionCount % 4===0) {
+            setMode("long"); 
+        } else{ 
+            setMode("short");
+        }
+        } else {
+            setMode ("Working Time")
+        }
+    }
+function setMode(newMode) {
+    mode = newMode;
+
+    if (mode=== "Working Time") timeLeft = 1500; //25 min
+    if (mode==="short") timeLeft = 300; // 5 min
+    if (mode==="long") timeLeft = 900; // 15 min 
+
+    modeLabel.textContent = mode.toUpperCase();
+    updateDisplay();
+}
+document.body.classList.add("finished");
+
+setTimeout(() => {
+    document.body.classList.remove("finished");
+}, 3000);
+startBtn.addEventListener('click',() => {
+    if(isRunning) {
+        pauseTimer();
+        startBtn.textContent = 'Start';
+    }else{
+        startTimer();
+        startBtn.textContent ='Pause';
+    }
+});
+resetBtn.addEventListener('click', resetTimer);
+updateDisplay();
